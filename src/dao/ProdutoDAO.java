9c7aee42feebdc3589e97a897fbe5a1df52bbcd9
Felipe Silva Honorato <felipeshonorato@gmail.com>
2022-01-19 17:43:04 -0300

    Foreign keys and inner join

--- src/dao/ProdutoDAO.java
+++ src/dao/ProdutoDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import modelo.Categoria;
 import modelo.Produto;
 
 import javax.xml.transform.Result;
@@ -40,6 +41,29 @@ public class ProdutoDAO {
         String sql = "SELECT ID, NOME, DESCRICAO FROM PRODUTO";
 
         try(PreparedStatement acao = connection.prepareStatement(sql)){
+
+            acao.execute();
+
+            try(ResultSet rst = acao.getResultSet()){
+                while (rst.next()){
+
+                    Produto produto = new Produto(rst.getInt(1), rst.getString(2), rst.getString(3));
+
+                    produtos.add(produto);
+                }
+            }
+        }return produtos;
+    }
+
+    public List<Produto> buscar(Categoria ct)  throws SQLException{
+        List<Produto> produtos = new ArrayList<Produto>();
+
+        System.out.println("Executando a query de buscar produto por categoria");
+
+        String sql = "SELECT ID, NOME, DESCRICAO FROM PRODUTO WHERE CATEGORIA_ID = ?";
+
+        try(PreparedStatement acao = connection.prepareStatement(sql)){
+            acao.setInt(1, ct.getId());
 
             acao.execute();
 
